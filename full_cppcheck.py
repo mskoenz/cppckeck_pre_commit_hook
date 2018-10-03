a386d2fcf99d742112587ac8a9c8de269ab56b47
mskoenz <mskoenz@gmx.net>
2018-10-03 20:28:48 +0200

    added python file

---
+++ full_cppcheck.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:  Mario S. Könz <mskoenz@gmx.net>
+
+import sys
+
+def main():
+    print("bla")
+    exit(1)
+
+if __name__ == "__main__":
+    main()
